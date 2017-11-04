be4c83b3fb986769c221642372871895f91b989c
thienn <thien.oslo@gmail.com>
2017-11-05 00:21:11 +0100

    Refactor - Making userInput method more robust
    + Making sure the program doesn’t crash when invalid input, also
    reporting back if it’s invalid so user can input another
    + More user-friendly so user can run it over and over until they exit.

--- src/main/java/no/westerdals/Client.java
+++ src/main/java/no/westerdals/Client.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -40,7 +41,6 @@ public class Client {
         }
     }
 
-
     public Connection getConnection() throws SQLException {
         MysqlDataSource ds = new MysqlDataSource();
         ds.setDatabaseName("myDB");
@@ -52,46 +52,42 @@ public class Client {
 
     }
 
-
-
     void userInput() {
         int values;
+        int check;
         Scanner input = new Scanner(System.in);
         System.out.println("If you want all of the subjects out type 1, if you want a specific one type 2: ");
-        values = input.nextInt();
-        if(values == 1) {
-            readTable();
-        } else if (values == 2) {
-            System.out.println("Which subject do you want - (Use subjectid)? ");
-            String emnekode = input.next();
-            try {
-
-                // Next make sure this goes into a loop, if invalid, report back to user, then ask for another value
-                String subjectid = emnekode;
-                PreparedStatement prepStmt = con.prepareStatement("select * from EMNER where subjectid = ?");
-                prepStmt.setString(1, subjectid);
-                ResultSet rs = prepStmt.executeQuery();
-                /*
-                while (rs.next()) {
-                    String name = rs.getString("name");
-                    subjectid = rs.getString("subjectid");
-                    String lecturer = rs.getString("lecturer");
-                    String starttime = rs.getString("starttime");
-                    String endtime = rs.getString("endtime");
-
-                    System.out.println("Emnenavn: " + name + " Emnekode: " + subjectid + " Foreleser: " + lecturer + " Startdato: " + starttime + " Sluttdato: " + endtime);
-                } */
-                readTablePrint(rs);
-                System.out.println("Table read - Finished");
-            } catch (SQLException e) {
-                e.printStackTrace();
+        try {
+            values = input.nextInt();
+            if(values == 1) {
+                readTable();
+            } else if (values == 2) {
+                System.out.println("Which subject do you want - (Use subjectid)? ");
+                String emnekode = input.next();
+                try {
+                    // Next make sure this goes into a loop, if invalid, report back to user, then ask for another value
+                    String subjectid = emnekode;
+                    PreparedStatement prepStmt = con.prepareStatement("select * from EMNER where subjectid = ?");
+                    prepStmt.setString(1, subjectid);
+                    ResultSet rs = prepStmt.executeQuery();
+                    readTablePrint(rs);
+                    System.out.println("Want to check again? 1 for yes, 2 for exit");
+                    check = input.nextInt();
+                    if(check == 1) {
+                        userInput();
+                    } else if (check == 2) {
+                        System.exit(0);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Input invalid - need to be 1 or 2");
+            userInput();
         }
-
     }
-
-    /* Create */
-
+    
     void createTable() {
         try {
             String q = "CREATE TABLE EMNER(" + "name varchar(50),"
@@ -109,7 +105,6 @@ public class Client {
         }
     }
 
-    // Drop function for development process
     void dropTable() {
         try {
             Statement stmt = con.createStatement();
@@ -126,18 +121,7 @@ public class Client {
         try{
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM EMNER");
-     /*       while(rs.next()) {
-                String name = rs.getString("name");
-                String subjectid = rs.getString("subjectid");
-                String lecturer = rs.getString("lecturer");
-                String starttime = rs.getString("starttime");
-                String endtime = rs.getString("endtime");
-
-                System.out.println("Emnenavn: " + name + " Emnekode: " + subjectid + " Foreleser: " + lecturer + " Startdato: " + starttime + " Sluttdato: " + endtime );
-                */
-          //  }
             readTablePrint(rs);
-            System.out.println("Table read - Finished");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -205,10 +189,8 @@ public class Client {
             } finally {
 
             }
-
         } catch (SQLException e) {
-
+            e.printStackTrace();
         }
-
     }
 }
